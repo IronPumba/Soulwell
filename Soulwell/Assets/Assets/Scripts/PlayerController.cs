872ce1f8b014300574c2c03909a7ea6a39a2ae4a
IronPumba <jamieproudlock@bigpond.com>
2015-03-03 18:33:14 +1000

    More level changes
    - Github is a heap of shit
    - Github created two asset folders
    - Use Asset Folder inside Asset Folder
    - WTF
    - Level Changed
    - Collider invisible walls created
    - Collider walls tested

---
+++ Soulwell/Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	float speed = 2.0f;
+	Vector3 pos;
+	Transform tr;
+	
+	void Start() {
+		pos = transform.position;
+		tr = transform;
+	}
+	
+	void Update() {
+		
+		if (Input.GetKey(KeyCode.D) && tr.position == pos)
+		{
+			pos += Vector3.right;
+		}
+		else if (Input.GetKey(KeyCode.A) && tr.position == pos)
+		{
+			pos += Vector3.left;
+		}
+		else if (Input.GetKey(KeyCode.W) && tr.position == pos)
+		{
+			pos += Vector3.up;
+		}
+		else if (Input.GetKey(KeyCode.S) && tr.position == pos)
+		{
+			pos += Vector3.down;
+		}
+		
+		transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+	}  
+}
