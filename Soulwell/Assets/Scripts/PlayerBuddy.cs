f8ff0175eb1f8eff929c5c3c96b37558bda6b71d
Richard Stein <105014@student.sae.edu.au>
2015-03-04 10:29:09 +1000

    Changed gitignore (again
    and set up a player buddy but untested as yet

---
+++ Soulwell/Assets/Scripts/PlayerBuddy.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBuddy : MonoBehaviour 
+{
+	public GameObject target;
+
+	public float followDistance;
+
+	public float travelSpeed;
+
+    public float dampTime;
+
+	public float jumpHeight;
+
+	public Transform lake;
+
+	public float distanceToLake;
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		transform.position = target.transform.position - new Vector3 (-2, -2, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        if(target)
+        {
+            if(Vector3.Distance(target.transform.position, transform.position) > followDistance)
+            {
+                transform.position += transform.forward * travelSpeed * Time.deltaTime;
+
+            }
+			else
+			{
+				transform.position += transform.forward * travelSpeed * 0;
+			}
+        }
+
+		if(rigidbody.velocity.y == 0 && Input.GetKey(KeyCode.Space))
+		{
+           
+			rigidbody.AddForce (new Vector3 (0, jumpHeight, 0));
+		}
+
+		if(Vector3.Distance(lake.transform.position, transform.position) < distanceToLake)
+		{
+			//race off to the lake and disappear
+		}
+	}
+}
