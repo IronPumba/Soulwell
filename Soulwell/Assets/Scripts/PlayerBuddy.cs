0fda975b63f9329de6b111b0136de13a4dc0ee35
Richard Stein <105014@student.sae.edu.au>
2015-03-05 16:54:40 +1000

    Finished the Buddy script
    It will follow the players jump etc, move within the distance you set to
    the player (target) and when you get to a certain distance to the lake
    transform, that you set, the buddy will head off to the lake and then
    disappear when it gets to within 0.5 of the lake transform.  If you need
    it to run faster to the lake, then include a multiplyer in that
    statement so that it goes faster.

--- Soulwell/Assets/Scripts/PlayerBuddy.cs
+++ Soulwell/Assets/Scripts/PlayerBuddy.cs
@@ -17,7 +17,7 @@ public class PlayerBuddy : MonoBehaviour
 
 	public float distanceToLake;
 
-
+	private bool freedom = true;
 
 	// Use this for initialization
 	void Start () 
@@ -30,14 +30,16 @@ public class PlayerBuddy : MonoBehaviour
 	{
         if(target)
         {
-            if(Vector3.Distance(target.transform.position, transform.position) > followDistance)
+            if(Vector3.Distance(target.transform.position, transform.position) > followDistance && freedom)
             {
-                transform.position += transform.forward * travelSpeed * Time.deltaTime;
-
+				Vector3 dir = target.transform.position - transform.position;
+				dir.Normalize();
+                transform.position += dir * travelSpeed * Time.deltaTime;
+			
             }
 			else
 			{
-				transform.position += transform.forward * travelSpeed * 0;
+				transform.position += transform.forward * 0;
 			}
         }
 
@@ -49,7 +51,19 @@ public class PlayerBuddy : MonoBehaviour
 
 		if(Vector3.Distance(lake.transform.position, transform.position) < distanceToLake)
 		{
-			//race off to the lake and disappear
+			//race off to the lake 
+			freedom = false;
+			Vector3 newDir = lake.transform.position - transform.position;
+			newDir.Normalize();
+			transform.position += newDir * travelSpeed * Time.deltaTime;
+		}
+
+		if (Vector3.Distance(lake.transform.position, transform.position) < 0.5f)
+		{
+			//and disappear
+			gameObject.renderer.enabled = false;
+			//if you need to reuse this guy later, then I would suggest //ing out the following line.
+			gameObject.SetActive = false;
 		}
 	}
 }
