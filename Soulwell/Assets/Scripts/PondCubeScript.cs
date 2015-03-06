82e48dacdc01a5a91f0c831659fbc3e77e98b222
IronPumba <jamieproudlock@bigpond.com>
2015-03-06 17:41:06 +1000

    Coding Changes
    - PondCubeScript now changes all blocks in the pond to the colour red
    OnTriggerEnter

---
+++ Soulwell/Assets/Scripts/PondCubeScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PondCubeScript : MonoBehaviour {
+
+	void OnTriggerEnter (Collider info)
+	{
+		if(info.tag == "Player") renderer.material.color = Color.red;
+	}
+}
