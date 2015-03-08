a41897bddce207e980f7851c9bf85cb0886cd8d6
IronPumba <jamieproudlock@bigpond.com>
2015-03-08 19:51:28 +1000

    Title Screen
    - Functional Title Screen Added
    - Fade screen script added
    - Buddy Character Re-enabled

---
+++ Soulwell/Assets/Scripts/Move.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public float speed = 1.0f;
+	
+	// Update is called once per frame
+	//Moves objects towards playerObject
+	void Update () 
+	{
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		if (player) 
+		{
+			Vector3 moveDirection = (player.transform.position - transform.position).normalized;
+			transform.position += moveDirection * speed * Time.deltaTime;
+		}
+	}
+
+}
